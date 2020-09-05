44dc2368b9cc43b31356115086b99dc29b084138
tnwjd2050 <tnwjd2050@naver.com>
2020-09-05 21:08:56 +0900

    포트 변수화

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require("express");
 const app = express();
 const router = require("./router/main")(app);
+const PORT = 8080;
 
 app.set("views", __dirname + "/views");
 app.set("view engine", "ejs");
 app.engine("html", require("ejs").renderFile);
 
-const server = app.listen(8080, function () {
-  console.log("Express server has started on port 8080");
+const server = app.listen(PORT, function () {
+  console.log(`Express server has started on port${PORT}`);
 });
 
 app.use(express.static('public'));
