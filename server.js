c328139d1f0b8ba3e4af85f62d276370073bc029
tnwjd2050 <tnwjd2050@naver.com>
2020-09-06 20:54:56 +0900

    ejs/sass 환경 추가

--- server.js
+++ server.js
@@ -1,14 +1,28 @@
+// Initialize the app
 const express = require("express");
 const app = express();
-const router = require("./router/main")(app);
+const sassMiddleware = require("node-sass-middleware");
 const PORT = 8080;
 
-app.set("views", __dirname + "/views");
+app.set("views", "./views");
 app.set("view engine", "ejs");
-app.engine("html", require("ejs").renderFile);
 
-const server = app.listen(PORT, function () {
-  console.log(`Express server has started on port${PORT}`);
+// Setup SASS directories
+const path = require('path');
+app.use(sassMiddleware({
+    src: __dirname + '/sass', 
+    dest: __dirname + '/public/stylesheets', 
+    debug: true, 
+    outputStyle: 'compressed' 
+  }),
+  // The static middleware must come after the sass middleware
+  express.static(path.join(__dirname, 'public'))
+)
+
+app.get("/", function (req, res) {
+  res.render("video");
 });
 
-app.use(express.static('public'));
+// Listen for incoming requests and serve them.
+app.listen(process.env.PORT || PORT);
+console.log("Server started on " + PORT);
