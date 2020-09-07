ea2feaa0bce8eedfba29db493767fbcf2ae7adb1
tnwjd2050 <tnwjd2050@naver.com>
2020-09-07 21:12:48 +0900

    es6/sass/ejs 셋팅

--- server.js
+++ server.js
@@ -1,23 +1,12 @@
 // Initialize the app
 const express = require("express");
 const app = express();
-const sassMiddleware = require("node-sass-middleware");
 const PORT = 8080;
 
 app.set("views", "./views");
 app.set("view engine", "ejs");
 
-// Setup SASS directories
-const path = require('path');
-app.use(sassMiddleware({
-    src: __dirname + '/sass', 
-    dest: __dirname + '/public/stylesheets', 
-    debug: true, 
-    outputStyle: 'compressed' 
-  }),
-  // The static middleware must come after the sass middleware
-  express.static(path.join(__dirname, 'public'))
-)
+app.use("/public", express.static("public"));
 
 app.get("/", function (req, res) {
   res.render("video");
