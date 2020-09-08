0eb8821cd76ebd21aec6436d159b614f2d35726e
tnwjd2050 <tnwjd2050@naver.com>
2020-09-08 23:43:25 +0900

    볼륨 조절 기능 추가

--- assets/js/video.js
+++ assets/js/video.js
@@ -5,6 +5,7 @@ const volumeBtn = document.getElementById('jsVolumeBtn');
 const fullScrnBtn = document.getElementById('jsFullScreen');
 const currentTime = document.getElementById("currentTime");
 const totalTime = document.getElementById("totalTime");
+const volumeRange = document.getElementById("jsVolume");
 
 function handlePlayClick() {
   if (videoPlayer.paused) {
@@ -20,9 +21,11 @@ function handleVolumeClick() {
   if (videoPlayer.muted) {
     videoPlayer.muted = false;
     volumeBtn.innerHTML = '<i class="fas fa-volume-up">소리 켜기</i>';
+    volumeRange.value = videoPlayer.volume;
   } else {
     videoPlayer.muted = true;
     volumeBtn.innerHTML = '<i class="fas fa-volume-mute">소리 끄기</i>';
+    volumeRange.value = 0;
   }
 }
 
@@ -92,13 +95,37 @@ function handleEnded() {
   videoPlayer.currentTime = 0;
 }
 
+function handleDrag(event) {
+  const {
+    target: {
+      value
+    }
+  } = event;
+
+  const volumeBtnIcon = volumeBtn.querySelector('.fas');
+
+  videoPlayer.volume = value;
+  volumeBtnIcon.className = "";
+  volumeBtnIcon.classList.add('fas');
+
+  if (value >= 0.6) {
+    volumeBtnIcon.classList.add('fa-volume-up')
+  } else if (value >= 0.2) {
+    volumeBtnIcon.classList.add('fa-volume-down')
+  } else {
+    volumeBtnIcon.classList.add('fa-volume-off')
+  }
+}
+
 function init() {
   videoPlayer = videoContainer.querySelector('video');
+  videoPlayer.volume = 0.5;
   playBtn.addEventListener('click', handlePlayClick);
   volumeBtn.addEventListener('click', handleVolumeClick);
   fullScrnBtn.addEventListener('click', goFullScreen);
   videoPlayer.addEventListener("loadedmetadata", setTotalTime);
   videoPlayer.addEventListener("ended", setTotalTime);
+  volumeRange.addEventListener("input", handleDrag);
 }
 
 if (videoContainer) {
