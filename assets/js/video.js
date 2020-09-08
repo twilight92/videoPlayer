5da7eb8cf433c6d6ffafde2fcef5cd599d68e1d3
tnwjd2050 <tnwjd2050@naver.com>
2020-09-08 21:27:09 +0900

    재생 정지 기능 추가

--- assets/js/video.js
+++ assets/js/video.js
@@ -1,18 +1,21 @@
 const videoContainer = document.getElementById('jsVideoPlayer');
 let videoPlayer;
-const playBtn =  document.getElementById('jsPlayButton');
+const playBtn =  document.getElementById('jsPlayBtn');
 
 function handlePlayClick() {
-    if (videoPlayer.paused) {
-        videoPlayer.play();
-    } else {
-        videoPlayer.pause();
-    }
+  if (videoPlayer.paused) {
+    videoPlayer.play();
+    playBtn.innerHTML = '<i class="fas fa-pause">재생</i>';
+  } else {
+    videoPlayer.pause();
+    playBtn.innerHTML = '<i class="fas fa-play">정지</i>';
+  }
+}
 }
 
 function init() {
-    videoPlayer = videoContainer.querySelector('video');
-    playBtn.addEventListener('click', handlePlayClick);
+  videoPlayer = videoContainer.querySelector('video');
+  playBtn.addEventListener('click', handlePlayClick);
 }
 
 if (videoContainer) {
